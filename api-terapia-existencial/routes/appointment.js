7008a442274ddf1db9646a7645136eb9b971213c
Eliana Ughetto <elianaughetto10@gmail.com>
2024-02-14 16:16:27 +0100

    again changed the code for vercel to work

--- api-terapia-existencial/routes/appointment.js
+++ api-terapia-existencial/routes/appointment.js
@@ -15,10 +15,4 @@ route.get('/appointments/:userId', appointmentController.getAppointmentByUserId)
 
 route.delete('/appointment/:userId/:appointmentId', appointmentController.deleteUserAppointment)
 
-//trae un comentario por id
-//route.get('/feedback/:id', feedbackController.getFeedbackById)
-//trae los comentarios por servicio
-//route.get('/feedbacks/:serviceId', feedbackController.getFeedbackByServiceId)
-//route.put('/feedback/:id', feedbackController.updateFeedback)
-
 export default route;
